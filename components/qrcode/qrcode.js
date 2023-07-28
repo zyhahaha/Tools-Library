e92a38a40aaba95d0fc5a88672ac270638199157
zhaoyang <zhaoyang@lyky.cn>
2023-07-28 16:14:44 +0800

    update

--- components/qrcode/qrcode.js
+++ components/qrcode/qrcode.js
@@ -11,15 +11,23 @@ Component({
      * 组件的初始数据
      */
     data: {
-        qrTxt: 'https://github.com/liuxdi/wx-qr'
+        inputValue: '',
+        qrTxt: ''
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
+        onChangeValue(e) {
+            this.setData({
+                inputValue: e.detail.value
+            })
+        },
         onGenerateQrcode() {
-
+            this.setData({
+                qrTxt: this.data.inputValue
+            })
         }
     }
 })
