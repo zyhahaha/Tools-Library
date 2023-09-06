2a7194f504ad996c10c394434ec161586c18ad62
zhaoyang <zhaoyang@lyky.cn>
2023-09-06 09:27:42 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,21 @@ const app = getApp()
 
 Page({
   data: {
-    tabValue: '0'
+    drawerVisible: false,
+    tabName: '房贷计算器（等额本息）',
+    tabValue: '0',
+    sideMenuList: [
+      {
+        title: '房贷计算器（等额本息）',
+        index: '0'
+      }, {
+        title: '条形码 / 二维码 / 解析',
+        index: '1'
+      }, {
+        title: '解析',
+        index: '2'
+      }
+    ]
     // tabPanelstyle: 'display:flex;justify-content:center;align-items:center;',
   },
   onLoad() {
@@ -14,17 +28,28 @@ Page({
       })
     }
   },
-  onHandlerDdvanced() {
-    console.log('xxx')
+  onOpenDrawer() {
+    this.setData({
+      drawerVisible: true
+    })
+  },
+  onClickDrawerItem(e) {
+    this.setData({
+      tabValue: e.detail.item.index,
+      tabName: e.detail.item.title
+    })
+    this.setData({
+      drawerVisible: false
+    })
   },
   onTabsChange(event) {
     this.setData({
       tabValue: event.detail.value,
     });
-    console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
+    // console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
   },
 
   onTabsClick(event) {
-    console.log(`Click tab, tab-panel value is ${event.detail.value}.`);
+    // console.log(`Click tab, tab-panel value is ${event.detail.value}.`);
   },
 })
