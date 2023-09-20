a2200733c384b3523ed3f9811dfff47b90642aa0
zhaoyang <zhaoyang@lyky.cn>
2023-09-20 17:30:32 +0800

    update

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+import * as paddlejs from '@paddlejs/paddlejs-core'
+import '@paddlejs/paddlejs-backend-webgl'
+const plugin = requirePlugin('paddlejs-plugin')
+plugin.register(paddlejs, wx)
+
+const initPaddle = () => {
+  return new Promise((resolve, reject) => {
+    const pdjs = new paddlejs.Runner({
+      modelPath: 'https://paddlejs.bj.bcebos.com/models/fuse/humanseg/humanseg_398x224_fuse_activation/model.json',
+      feedShape: {
+        fw: 224,
+        fh: 224
+      },
+      fill: '#fff',
+      targetSize: {
+        height: 224,
+        width: 224
+      },
+      mean: [0.485, 0.456, 0.406],
+      std: [0.229, 0.224, 0.225]
+    })
+    pdjs.init().then(() => {
+     // 初始化完成
+      console.log('初始化成功')
+      resolve(pdjs)
+    })
+  })
+}
+
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
   globalData: {
-    userInfo: null
+    paddleRunner: null
+  },
+  async onLaunch() {
+    const paddleRunner = await initPaddle()
+    this.globalData.paddleRunner = paddleRunner
   }
 })
--- components/barcode/barcode.js
+++ components/barcode/barcode.js
@@ -1,3 +1,4 @@
+const app = getApp();
 Component({
     /**
      * 组件的属性列表
@@ -37,6 +38,77 @@ Component({
                     })
                 }
             })
-        }
+        },
+        chooseImage() {
+            const that = this
+            // this.getImageInfo()
+            wx.chooseImage({
+                count: 1,
+                sizeType: ['original'],
+                sourceType: ['album', 'camera'],
+                success(res) {
+                    // console.log(res.tempFilePaths)
+                    // tempFilePath可以作为img标签的src属性显示图片
+                    that.getImageInfo(res.tempFilePaths[0]);
+                }
+            });
+        },
+        getImageInfo(imgPath) {
+            // wx.getFileSystemManager()
+            const fs = wx.getFileSystemManager()
+            // 获取到图片的像素信息
+            // console.log(fs)
+            fs.readFile({
+                filePath: imgPath,
+                // encoding: 'utf8',
+                position: 0,
+                success: async res => {
+                    // console.log(new Uint8Array(res.data))
+                    // 创建离屏 2D canvas 实例
+                    const canvas = wx.createOffscreenCanvas({type: '2d', width: 300, height: 150})
+                    // 获取 context。注意这里必须要与创建时的 type 一致
+                    const context = canvas.getContext('2d')
+
+                    // 创建一个图片
+                    const image = canvas.createImage()
+                    // 等待图片加载
+                    await new Promise(resolve => {
+                        image.onload = resolve
+                        image.src = imgPath // 要加载的图片 url
+                    })
+
+                    // 把图片画到离屏 canvas 上
+                    context.clearRect(0, 0, 300, 150)
+                    context.drawImage(image, 0, 0, 300, 150)
+
+                    // 获取画完后的数据
+                    const imgData = context.getImageData(0, 0, 300, 150)
+                    console.log('xxx', imgData)
+                    // imgData.data = new Uint8Array(imgData.data)
+                    // this.predict(imgData)
+                    this.predict({
+                        data: new Uint8Array(imgData.data),
+                        width: imgData.width,
+                        height: imgData.height
+                    });
+                },
+                fail(res) {
+                    // console.error(res)
+                }
+            })
+        },
+        predict(imgObj) {
+            // 4. 在线预测计算
+            // console.log('xxx', app.globalData.paddleRunner.predict)
+            const pdjs = app.globalData.paddleRunner
+            pdjs.predict(imgObj, data => {
+                console.log('xxx', data)
+                // 5. 对预测结果进行后处理
+                const maxItem = pdjs.utils.getMaxItem(data);
+                this.setData({
+                    result: maps[maxItem.index]
+                });
+            });
+        },
     }
 })
